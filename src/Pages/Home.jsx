fd7a43314036ac723ff6deb10e7ca4a7a089fe6a
KakuElvis <kakuelvis@gmail.com>
2025-07-17 11:01:25 +0100

    updated landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
 
     <>
-        <div className='bg-gray-10 py-20 px-40'>
+        <div className='bg-gray-10 py-20 '>
 
             <header>
                 <nav className="px-40 flex justify-between items-center bg-white  shadow-md fixed top-0 left-0 right-0 z-50">
@@ -34,7 +34,7 @@ const Home = () => {
                 </nav>
             </header>
 
-            <section className=' items-center justify-center'>
+            <section className=' items-center justify-center px-40'>
                 <div className='flex flex-col  items-center justify-between z-20 md:flex-row'>
                     <div>
                         <div>
@@ -58,7 +58,7 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className=''>
+            <section className='px-40'>
                 <div>
                     <h1 className='text-black text-5xl font-bold'> <span className='text-heading'>Choose better</span> <br/> with Sika</h1>
                 </div>
@@ -78,7 +78,7 @@ const Home = () => {
                 </div>
             </section>
 
-            <section>
+            <section className='px-40'>
                 <div className='flex flex-col  items-center justify-between z-20 md:flex-row'>
                     <div>
                         <div className='mb-10'>
@@ -101,22 +101,24 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className=''>
-                <div>
-                    <h1 className='text-black text-5xl font-bold'>Building our <span className='text-heading'> <br />expertise, for you</span> </h1>
-                </div>
-                <div className='flex gap-2 py-5'>
-                    <div className='bg-green-200  p-10 rounded-lg'>
-                        <h4 className='mb-5 font-bold'>Protect what matters</h4>
-                        <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempora ipsam adipisci excepturi cum magni commodi neque. Eveniet, repudiandae cumque?</p>
+            <section className='bg-gray-50 py-5'>
+                <div className='px-40'>
+                    <div>
+                        <h1 className='text-black text-5xl font-bold'>Building our <span className='text-heading'> <br />expertise, for you</span> </h1>
                     </div>
-                    <div className='bg-blue-500  p-10 rounded-lg text-white'>
-                        <h4 className='mb-5 font-bold'>Nurture it to grow</h4>
-                        <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempora ipsam adipisci excepturi cum magni commodi neque. Eveniet, repudiandae cumque?</p>
-                    </div>
-                    <div className='bg-yellow-500  p-10 rounded-lg'>
-                        <h4 className='mb-5 font-bold'>Many means to one goal</h4>
-                        <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempora ipsam adipisci excepturi cum magni commodi neque. Eveniet, repudiandae cumque?</p>
+                    <div className='flex gap-2 py-5'>
+                        <div className='bg-green-200  p-10 rounded-lg'>
+                            <h4 className='mb-5 font-bold'>Protect what matters</h4>
+                            <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempora ipsam adipisci excepturi cum magni commodi neque. Eveniet, repudiandae cumque?</p>
+                        </div>
+                        <div className='bg-blue-500  p-10 rounded-lg text-white'>
+                            <h4 className='mb-5 font-bold'>Nurture it to grow</h4>
+                            <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempora ipsam adipisci excepturi cum magni commodi neque. Eveniet, repudiandae cumque?</p>
+                        </div>
+                        <div className='bg-yellow-500  p-10 rounded-lg'>
+                            <h4 className='mb-5 font-bold'>Many means to one goal</h4>
+                            <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempora ipsam adipisci excepturi cum magni commodi neque. Eveniet, repudiandae cumque?</p>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -125,25 +127,137 @@ const Home = () => {
                 <div className='space-y-2'>
                     <h1 className='font-bold text-5xl'>World of <span className='text-heading'>Sika</span></h1>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, sequi.</p>
-                    <button className='bg-heading text-white p-2 rounded-sm font-bold'>Join Sika</button>
+                    <button className='bg-heading text-white py-2 px-3 rounded-sm font-bold'>Join Sika</button>
                 </div>
                 <div className=''>
                     <img src="./src/assets/phone.png" className="transform scale-x-[-1] py-5 items-center" alt="" width="500" height="300"/>
                 </div>
                 <div className='flex flex-row gap-5 '>
                     <div>
-                        <h2 className='text-xl font-bold'>60+</h2>
+                        <h2 className='text-4xl font-bold'>60+</h2>
                         <h4>hi</h4>
                     </div>
                     <div>
-                        <h2 className='text-xl font-bold'>60+</h2>
+                        <h2 className='text-4xl font-bold'>60+</h2>
                         <h4>hi</h4>
                     </div>
                     <div>
-                        <h2 className='text-xl font-bold'>60+</h2>
+                        <h2 className='text-4xl font-bold'>60+</h2>
                         <h4>hi</h4>
                     </div>
                 </div>
+            </section>
+
+            <section className='px-40'>
+                <div>
+                    <h1 className='text-black text-5xl font-bold'>Let's make your <br/> <span className='text-heading'>wealth work for you</span> </h1>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ullam omnis nam perspiciatis dolorem ipsum velit, dolores voluptates consectetur deleniti.</p>
+                </div>
+                <div className='flex space-x-5 py-5'>
+                    <div className=' space-y-5 p-10 rounded-lg  gap-10 w-1/3'>
+                        <div>
+                            <h4 className='font-bold text-2xl'>Protect what matters</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                        </div>
+                        <div>
+                            <h4 className='font-bold text-2xl'>Protect what matters</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                        </div>
+                    </div>
+                   <div className='w-1/3'>
+                        <img src="./src/assets/phone.png" className="transform scale-x-[-1] py-5 items-center" alt="" width="400" height="200"/>
+                    </div>
+                    <div className=' space-y-5 p-10 rounded-lg gap-10 w-1/3'>
+                        <div>
+                            <h4 className='font-bold text-2xl'>Protect what matters</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                        </div>
+                        <div>
+                            <h4 className='font-bold text-2xl'>Protect what matters</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='px-40 bg-heading py-5'>
+                <div className='flex space-x-5'>
+                    <div className='py-5 space-y-5'>
+                        <h2 className='text-white text-2xl font-bold'>Trused <br /> partnership</h2>
+                        <button className='bg-white text-heading py-2 px-3 rounded-sm font-bold'>Join Sika</button>
+                        <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                    </div>
+                    <div className='flex space-x-5 py-5 justify-between'>
+                        <div className='bg-white p-10 rounded-lg  gap-10'>
+                            <div>
+                                <h4 className='font-bold'>Protect what matters</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                            </div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                        <div className='bg-white p-10 rounded-lg  gap-10'>
+                            <div>
+                                <h4 className='font-bold'>Protect what matters</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                            </div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                        <div className='bg-white p-10 rounded-lg  gap-10'>
+                            <div>
+                                <h4 className='font-bold'>Protect what matters</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam fuga quod officia quae corporis aperiam non assumenda veniam quam?</p>
+                            </div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='px-40 py-5'>
+                    <div className='py-5 space-y-5'>
+                        <h2 className='text-heading text-3xl font-bold'>Every win inapires <br /> <span className='text-black'>us for better</span></h2>
+                    </div>
+                    <div className='flex flex-row space-x-2 py-5 justify-between'>
+                        <div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                         <div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                         <div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                         <div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                        
+                    </div>
+            </section>
+
+            <section className='bg-heading px-40 py-5'>
+                    <div className='py-5 space-y-5'>
+                        <h2 className='text-white text-3xl font-bold'>Every win inapires </h2>
+                    </div>
+                    <div className='flex space-x-40 justify-center items-center text-center'>
+                        <img src="./src/assets/sika.png" className="py-5" alt="" width="50" height="50"/>
+                        <img src="./src/assets/sika.png" className="py-5" alt="" width="50" height="50"/>
+                        <img src="./src/assets/sika.png" className="py-5" alt="" width="50" height="50"/>
+                        <img src="./src/assets/sika.png" className="py-5" alt="" width="50" height="50"/>
+                    </div>
+                    <div className='flex space-x-5'>
+                        <div>
+                            <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                        </div>
+                        <div className='space-y-2'>
+                            <h2 className=''>Managing your finance with us is a tap away</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, ut!</p>
+                            <div className='flex'>
+                                <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                                <img src="./src/assets/sika.png" className="py-5" alt="" width="100" height="100"/>
+                            </div>
+                        </div>
+                    </div>
+                        
             </section>
             
         </div>
